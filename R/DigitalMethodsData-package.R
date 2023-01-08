aeaba85dcf8f9701a2da2c0d21e3ab22e837d826
Amanda Regan <aeregan@clemson.edu>
2023-01-08 12:11:20 -0500

    add package documentation

---
+++ R/DigitalMethodsData-package.R
@@ -0,0 +1,12 @@
+#' Datasets for Digital Methods II at Clemson University
+#'
+#' This package provides sample datasets for the use of students in History 8510
+#' at Clemson University. It is similar to the \pkg{datasets} and the
+#' \pkg{histdata} package.
+#'
+#' @section References: References for each dataset included here is included on
+#' the documentation page.
+#'
+#' @name DigitalMethodsData
+#' @docType package
+NULL
--- R/almshouse_admissions.R
+++ R/almshouse_admissions.R
@@ -1,6 +1,6 @@
 #' Philadelphia Almshouse Admissions 1796-1803
 #'
-#' A sample of people admitted into the Philadelphia ALMSHOUSE 1796-1803.
+#' A sample of people admitted into the Philadelphia Almshouse 1796-1803.
 #' This contains a this is a sample from the Index, including last names that
 #' started A through G as well as a handful of people indicated by race. This
 #' data was created from and Index of Admissions to the Philadelphia Almshouse.
@@ -8,12 +8,12 @@
 #' of that record to create the dataset. See \href{https://repository.upenn.edu/mead/42/}
 #' {the original repository} for the dataset's codebook.
 
-#'
 #' @format One data frame with 201 variables and 3886 observations.
 #' @author Billy G. Smith
 #' @references This data is taken from the
 #'   \href{https://repository.upenn.edu/mead/42/}{Almshouse Admissions 1796-1803
 #'   repository at the Magazine of Early American Datasets}.
 #' @examples
+#' data(judges)
 #' head(judges)
 "almshouse_admissions"
